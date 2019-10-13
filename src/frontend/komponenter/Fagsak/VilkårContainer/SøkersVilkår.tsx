c20b7e8ae0a0771a7222d7d4acb4913f55e93688
Henning Håkonsen <henninghaakonsen@me.com>
2019-10-13 10:32:45 +0200

    Legger til siste del av søkers informasjonsbolk. Refaktorert ut vilkår bolk.

--- src/frontend/komponenter/Fagsak/VilkårContainer/SøkersVilkår.tsx
+++ src/frontend/komponenter/Fagsak/VilkårContainer/SøkersVilkår.tsx
@@ -4,6 +4,7 @@ import { datakilder } from '../../../typer/vilkår';
 import { hentBosattINorgeVilkår, hentOppholdINorge } from '../../../utils/vilkårHenting';
 import PersonNavnOgIkon from '../../Felleskomponenter/PersonNavnOgIkon/PersonNavnOgIkon';
 import Vilkår from '../../Felleskomponenter/Vilkår/Vilkår';
+import VilkårBolk from '../../Felleskomponenter/VilkårBolk/VilkårBolk';
 
 interface IProps {
     behandling: IBehandling;
@@ -14,32 +15,45 @@ const SøkersVilkår: React.StatelessComponent<IProps> = ({ behandling }) => {
         <div className={'vilkårperson'}>
             <PersonNavnOgIkon person={behandling.personopplysninger.søker} type={'søker'} />
 
-            <div className={'vilkårperson__informasjon'}>
-                <table className={'vilkårperson__informasjon_tabell'}>
-                    <Vilkår
-                        datakilde={datakilder.FOLKEREGISTERET}
-                        kortInfo={'Bosatt i Norge'}
-                        navn={'Bosted'}
-                        oppfylt={hentBosattINorgeVilkår(behandling.behandlingsresultat)}
-                    />
-                    <Vilkår
-                        datakilde={datakilder.SØKNAD}
-                        kortInfo={'Ja'}
-                        navn={'Opphold i Norge i de neste 12 mnd'}
-                        oppfylt={hentOppholdINorge(behandling.søknad)}
-                    />
-                    <Vilkår
-                        datakilde={datakilder.FOLKEREGISTERET}
-                        kortInfo={'Norsk'}
-                        navn={'Statsborgerskap'}
-                    />
-                    <Vilkår
-                        datakilde={datakilder.SØKNAD}
-                        kortInfo={'Nei'}
-                        navn={'Arbeid eller ytelser fra utlandet'}
-                    />
-                </table>
-            </div>
+            <VilkårBolk>
+                <Vilkår
+                    datakilde={datakilder.FOLKEREGISTERET}
+                    kortInfo={'Bosatt i Norge'}
+                    navn={'Bosted'}
+                    oppfylt={hentBosattINorgeVilkår(behandling.behandlingsresultat)}
+                />
+                <Vilkår
+                    datakilde={datakilder.SØKNAD}
+                    kortInfo={'Ja'}
+                    navn={'Opphold i Norge i de neste 12 mnd'}
+                    oppfylt={hentOppholdINorge(behandling.søknad)}
+                />
+                <Vilkår
+                    datakilde={datakilder.FOLKEREGISTERET}
+                    kortInfo={'Norsk'}
+                    navn={'Statsborgerskap'}
+                />
+                <Vilkår
+                    datakilde={datakilder.SØKNAD}
+                    kortInfo={'Nei'}
+                    navn={'Arbeid eller ytelser fra utlandet'}
+                />
+            </VilkårBolk>
+
+            <br />
+            <VilkårBolk tittel={'Medlemskap i Folketrygden'}>
+                <Vilkår
+                    datakilde={datakilder.FOLKEREGISTERET}
+                    kortInfo={'Botid i Norge'}
+                    navn={'5 år'}
+                    oppfylt={hentBosattINorgeVilkår(behandling.behandlingsresultat)}
+                />
+                <Vilkår
+                    datakilde={datakilder.MEDLEMSSKAPSREGISTERET}
+                    kortInfo={'Nei'}
+                    navn={'Funn i Medlemsskapsregisteret'}
+                />
+            </VilkårBolk>
         </div>
     );
 };
