8fc31ac2d1c5abf0ca39f3db3c36714f43342a6f
Henning Håkonsen <henninghaakonsen@me.com>
2019-11-15 08:57:02 +0100

    Byttet ut tredjepartsapi med slack sitt eget api slik at jeg får satt proxy config på clienten.

--- src/backend/slack/slack.ts
+++ src/backend/slack/slack.ts
@@ -1,8 +1,14 @@
+import { WebClient } from '@slack/web-api';
 import { Response } from 'express';
-import slack from 'slack';
+import HttpsProxyAgent from 'https-proxy-agent';
 import { SessionRequest } from '../auth/utils/session';
 import { logError, logInfo } from '../customLoglevel';
 import { namespace } from '../Environment';
+const token = process.env.SLACK_TOKEN;
+const proxy = new HttpsProxyAgent(process.env.http_proxy || 'http://webproxy.nais:8088');
+const slack = new WebClient(token, {
+    agent: process.env.NODE_ENV === 'production' ? proxy : undefined,
+});
 
 export const slackNotify = (req: SessionRequest, res: Response, kanal: string) => {
     const displayName = req.session.displayName ? req.session.displayName : 'System';
@@ -13,7 +19,6 @@ export const slackNotify = (req: SessionRequest, res: Response, kanal: string) =
         .postMessage({
             channel: `#${kanal}`,
             text: formatertMelding,
-            token: process.env.SLACK_TOKEN,
         })
         .then(() => res.status(200).send())
         .catch((error: Error) => {
