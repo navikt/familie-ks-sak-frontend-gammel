7adaa02e580e40ec24b36e892d9657fa0a70d20d
Henning Håkonsen <henninghaakonsen@me.com>
2019-11-15 09:32:22 +0100

    Justert config og lagt på litt mer logging.

--- src/backend/slack/slack.ts
+++ src/backend/slack/slack.ts
@@ -1,14 +1,21 @@
-import { WebClient } from '@slack/web-api';
+import { LogLevel, WebClient } from '@slack/web-api';
 import { Response } from 'express';
 import HttpsProxyAgent from 'https-proxy-agent';
 import { SessionRequest } from '../auth/utils/session';
 import { logError, logInfo } from '../customLoglevel';
 import { namespace } from '../Environment';
+
+const agent =
+    process.env.NODE_ENV !== 'production'
+        ? new HttpsProxyAgent('http://webproxy.nais:8088')
+        : undefined;
+
 const token = process.env.SLACK_TOKEN;
-const proxy = new HttpsProxyAgent(process.env.http_proxy || 'http://webproxy.nais:8088');
 const slack = new WebClient(token, {
-    agent: process.env.NODE_ENV === 'production' ? proxy : undefined,
+    agent,
+    logLevel: LogLevel.DEBUG,
 });
+console.log(slack, agent);
 
 export const slackNotify = (req: SessionRequest, res: Response, kanal: string) => {
     const displayName = req.session.displayName ? req.session.displayName : 'System';
