f5cbbf8cbb04d8a169d5e9b48541fa9885531e40
Henning Håkonsen <henninghaakonsen@me.com>
2019-11-18 10:09:50 +0100

    Prøver å bruke fetch istedet for axios.

--- src/backend/slack/slack.ts
+++ src/backend/slack/slack.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse, AxiosError } from 'axios';
-import { Response } from 'express';
+import { Response as ExpressResponse } from 'express';
+import fetch, { Response } from 'node-fetch';
 import { SessionRequest } from '../auth/utils/session';
 import { logError, logInfo } from '../customLoglevel';
 import { namespace } from '../Environment';
@@ -20,30 +21,48 @@ const axiosGjennomProxy =
 
 const token = process.env.SLACK_TOKEN;
 
-export const slackNotify = (req: SessionRequest, res: Response, kanal: string) => {
+export const slackNotify = (req: SessionRequest, res: ExpressResponse, kanal: string) => {
     const displayName = req.session.displayName ? req.session.displayName : 'System';
     const formatertMelding: string = `*${displayName}, ${namespace}*\n ${req.body.melding}`;
 
     logInfo(req, `Poster slack melding til #${kanal}: ${formatertMelding}`);
-    axiosGjennomProxy
-        .post(
-            'https://slack.com/api/chat.postMessage',
-            {
-                channel: `#${kanal}`,
-                text: formatertMelding,
+
+    const data = {
+        channel: `#${kanal}`,
+        text: formatertMelding,
+    };
+
+    fetch('https://slack.com/api/chat.postMessage', {
+        body: JSON.stringify(data),
+        headers: {
+            Accept: 'application/json',
+            'Accept-Charset': 'utf-8',
+            Authorization: `Bearer ${token}`,
+            'Content-type': 'application/json',
+        },
+        method: 'POST',
+    })
+        .then((response: Response) => {
+            res.status(response.status).send(response);
+        })
+        .catch((error: any) => {
+            console.log(error);
+            logError(req, `Sending av melding til slack feilet: ${error.stack}`);
+            res.status(error.response.status).send(error);
+        });
+
+    /*axiosGjennomProxy
+        .post('https://slack.com/api/chat.postMessage', data, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json; charset=utf-8',
             },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    'Content-Type': 'application/json; charset=utf-8',
-                },
-            }
-        )
+        })
         .then((response: AxiosResponse) => {
             res.status(200).send(response.data);
         })
         .catch((error: AxiosError) => {
             logError(req, `Sending av melding til slack feilet: ${error.stack}`);
             res.status(error.response.status).send(error);
-        });
+        });*/
 };
