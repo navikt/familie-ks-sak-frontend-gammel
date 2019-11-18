49aac5ae10c57012250b7f331a5034c19a1d5dbe
Henning Håkonsen <henninghaakonsen@me.com>
2019-11-18 12:48:48 +0100

    Setter opp forskjellige posts mot slack for å teste.

--- src/backend/slack/slack.ts
+++ src/backend/slack/slack.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse, AxiosError } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { Response as ExpressResponse } from 'express';
 import fetch, { Response } from 'node-fetch';
 import { SessionRequest } from '../auth/utils/session';
@@ -13,8 +13,9 @@ const axiosGjennomProxy =
     process.env.NODE_ENV !== 'development'
         ? axios.create({
               proxy: {
-                  host: 'webproxy-nais.nav.no',
+                  host: 'webproxy.nais',
                   port: 8088,
+                  protocol: 'http',
               },
           })
         : axios.create();
@@ -32,34 +33,60 @@ export const slackNotify = async (req: SessionRequest, res: ExpressResponse, kan
             text: formatertMelding,
         }),
         headers: {
-            Accept: 'application/json',
             'Content-type': 'application/json; charset=utf-8',
         },
         method: 'POST',
     })
         .then((response: Response) => {
-            return response.json();
+            return response.ok;
         })
         .catch((error: any) => {
-            console.log(error);
             logError(req, `Sending av melding til slack feilet: ${error.stack}`);
-            res.status(error.response.status).send(error);
+            return error;
         });
     console.log('post: ', postResponse);
-    res.status(200).send(postResponse);
 
-    /*axiosGjennomProxy
-        .post('https://slack.com/api/chat.postMessage', data, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json; charset=utf-8',
+    console.log('proxy: ', axiosGjennomProxy.defaults.proxy);
+    axiosGjennomProxy
+        .post(
+            'https://slack.com/api/chat.postMessage',
+            {
+                channel: `#${kanal}`,
+                text: formatertMelding,
             },
-        })
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+            }
+        )
         .then((response: AxiosResponse) => {
+            console.log('post med proxy med api: ', response.data);
+        })
+        .catch((error: AxiosError) => {
+            logError(req, `Sending av melding til slack via api med axios feilet: ${error.stack}`);
+        });
+
+    axiosGjennomProxy
+        .post(
+            process.env.SLACK_SENTRY_WEBHOOK,
+            {
+                text: formatertMelding,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+            }
+        )
+        .then((response: AxiosResponse) => {
+            console.log('post med proxy med hook: ', response.data);
             res.status(200).send(response.data);
         })
         .catch((error: AxiosError) => {
-            logError(req, `Sending av melding til slack feilet: ${error.stack}`);
+            logError(req, `Sending av melding til slack via hook med axios feilet: ${error.stack}`);
             res.status(error.response.status).send(error);
-        });*/
+        });
 };
